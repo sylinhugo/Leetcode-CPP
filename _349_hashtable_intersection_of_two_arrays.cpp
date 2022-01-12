3f30058d6c3561645851b074a43fbf9d458f31e5
Hugo <sylin.hugo@gmail.com>
2022-01-12 02:48:16 -0600

    Add time complexity

--- _349_hashtable_intersection_of_two_arrays.cpp
+++ _349_hashtable_intersection_of_two_arrays.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <unordered_set>
+#include <iostream>
 
 using namespace std;
 
@@ -21,8 +22,27 @@ public:
     }
 };
 
+/*
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
 int main()
 {
+    Solution sol;
+    vector<int> nums1 = {1, 2, 2, 1};
+    vector<int> nums2 = {2, 2};
+
+    auto res1 = sol.intersection(nums1, nums2);
+    for_each(res1.begin(), res1.end(), [&](const auto &e)
+             { cout << e << " "; });
+    cout << endl;
+
+    nums1 = {4, 9, 5};
+    nums2 = {9, 4, 9, 8, 4};
+    res1 = sol.intersection(nums1, nums2);
+    for_each(res1.begin(), res1.end(), [&](const auto &e)
+             { cout << e << " "; });
 
     return 0;
 }
