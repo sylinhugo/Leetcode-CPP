59b7c36e14a5e129fd2ebabf6e56fe95fa014cb4
Hugo <sylin.hugo@gmail.com>
2022-01-11 18:11:02 -0600

    First commit :))

---
+++ README.md
@@ -0,0 +1,3 @@
+# Breifly Introduction
+
+This project is for modern c++ practicing.
---
+++ _9_palindrome_number.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <iostream>
+
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+        int num = 0;
+        int aNum = std::abs(x);
+
+        while (aNum != 0)
+        {
+            int tmp = aNum % 10;
+            num = num * 10 + tmp;
+            aNum /= 10;
+        }
+
+        if (x >= 0 && x == num)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
+
+int main()
+{
+    Solution sol;
+    std::cout << sol.isPalindrome(121) << std::endl;
+    std::cout << sol.isPalindrome(-121) << std::endl;
+
+    return 0;
+}
