343b857acd127d2c372a226086cd91ee87fd43c9
Hugo <sylin.hugo@gmail.com>
2022-01-12 03:08:46 -0600

    Add new method and test case

--- _438_hashtable_find_all_anagram_in_a_string.cpp
+++ _438_hashtable_find_all_anagram_in_a_string.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
@@ -8,6 +9,38 @@ class Solution
 public:
     vector<int> findAnagrams(string s, string p)
     {
+        auto ns = s.length();
+        auto np = p.length();
+        unordered_map<char, int> wordP;
+        unordered_map<char, int> wordS;
+        vector<int> res;
+
+        for (auto i = 0; i != np; ++i)
+        {
+            wordP[p[i]] += 1;
+        }
+
+        for (auto i = 0; i != ns; ++i)
+        {
+            wordS[s[i]] += 1;
+
+            if (i >= np)
+            {
+                if (wordS[s[i - np]] == 1)
+                {
+                    wordS.erase(s[i - np]);
+                }
+                else
+                {
+                    wordS[s[i - np]] -= 1;
+                }
+            }
+            if (wordP == wordS)
+            {
+                res.push_back(i - np + 1);
+            }
+        }
+        return res;
     }
 };
 
@@ -37,12 +70,26 @@ public:
 
 /*
 Approach 2: Sliding Window
-Time Complexity:
-Space Complexity:
+Time Complexity: O(len(s) + len(p))
+Space Complexity: O(1)
 */
 
 int main()
 {
+    Solution sol;
+    string s1 = "cbaebabacd";
+    string p1 = "abc";
+
+    auto res1 = sol.findAnagrams(s1, p1);
+    for_each(res1.begin(), res1.end(), [&](int e)
+             { cout << e << " "; });
+    cout << endl;
+
+    s1 = "abab";
+    p1 = "ab";
+    res1 = sol.findAnagrams(s1, p1);
+    for_each(res1.begin(), res1.end(), [&](int e)
+             { cout << e << " "; });
 
     return 0;
 }
