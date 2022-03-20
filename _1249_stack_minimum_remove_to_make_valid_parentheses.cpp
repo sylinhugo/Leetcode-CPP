72ed970d3aa7d68bbceb72ae1d2ad0de43bd3ae0
Hugo Lin <sylin.hugo@gmail.com>
2022-03-20 02:09:42 -0500

    reformat the code

--- _1249_stack_minimum_remove_to_make_valid_parentheses.cpp
+++ _1249_stack_minimum_remove_to_make_valid_parentheses.cpp
@@ -1,70 +1,54 @@
-#include <string>
 #include <stack>
+#include <string>
 
 using namespace std;
 
-class Solution
-{
-public:
-    string minRemoveToMakeValid(string s)
-    {
-        int close = count(begin(s), end(s), ')');
-        int open = 0;
-        string ans;
+class Solution {
+ public:
+  string minRemoveToMakeValid(string s) {
+    int close = count(begin(s), end(s), ')');
+    int open = 0;
+    string ans;
 
-        for (char c : s)
-        {
-            if (c == '(')
-            {
-                if (open == close)
-                    continue;
-                ++open;
-            }
-            else if (c == ')')
-            {
-                --close;
-                if (open == 0)
-                    continue;
-                --open;
-            }
-            ans += c;
-        }
-        return ans;
+    for (char c : s) {
+      if (c == '(') {
+        if (open == close) continue;
+        ++open;
+      } else if (c == ')') {
+        --close;
+        if (open == 0) continue;
+        --open;
+      }
+      ans += c;
     }
+    return ans;
+  }
 };
 
-class Solution2
-{
-public:
-    string minRemoveToMakeValid(string s)
-    {
-        stack<int> Stack;
-        for (int i = 0; i < s.size(); i++)
-        {
-            if (s[i] == '(')
-            {
-                Stack.push(i);
-            }
-            else if (s[i] == ')')
-            {
-                if (Stack.empty())
-                    s[i] = ' ';
-                else
-                    Stack.pop();
-            }
-        }
-        while (!Stack.empty())
-        {
-            s[Stack.top()] = ' ';
-            Stack.pop();
-        }
-        string t;
-        for (auto ch : s)
-            if (ch != ' ')
-                t.push_back(ch);
-
-        return t;
+class Solution2 {
+ public:
+  string minRemoveToMakeValid(string s) {
+    stack<int> Stack;
+    for (int i = 0; i < s.size(); i++) {
+      if (s[i] == '(') {
+        Stack.push(i);
+      } else if (s[i] == ')') {
+        if (Stack.empty())
+          s[i] = ' ';
+        else
+          Stack.pop();
+      }
     }
+    while (!Stack.empty()) {
+      s[Stack.top()] = ' ';
+      Stack.pop();
+    }
+    string t;
+    for (auto ch : s)
+      if (ch != ' ') t.push_back(ch);
+
+    return t;
+  }
 };
 
 /*
@@ -77,8 +61,4 @@ Time Complexity:
 Space Complexity:
 */
 
-int main()
-{
-
-    return 0;
-}
+int main() { return 0; }
